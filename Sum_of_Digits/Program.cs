4e7645cd7a1a7eb43e88b5c7b56f4e79d2615210
Admin <Admin@DESKTOP-Q9C1DBG>
2022-04-01 19:58:58 +0530

    Add project files.

---
+++ Sum_of_Digits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum_of_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Any Number");
+            int n1 = int.Parse(Console.ReadLine());
+            int sum = 0; 
+            while (n1 != 0)
+            {
+                int rem = n1 % 10;
+                 sum = sum + rem;
+                n1 = n1 / 10;
+
+            }
+            Console.WriteLine("Sum of Digits is :"+sum);
+        }
+    }
+}
